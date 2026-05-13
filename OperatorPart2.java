db77fe3f5b3b57fa17ab8c878be81a83f11e4ef3
debianuser160 <debianuser160@gmail.com>
2026-05-13 16:13:22 +0100

    OperatorsPart 1, Part 2 and Precedence

---
+++ OperatorPart2.java
@@ -0,0 +1,31 @@
+public class OperatorPart2{
+	public static void main(String[] args) {
+		//Logical Operators(and(&&), or(||), not(!))
+		int num1 = 15;
+		int num2 = 30;
+		int num3 = 18;
+		
+		boolean andOperator = (num1 > num2) && (num1 > num3);
+		System.out.printf("is (%d > %d) && (%d > %d): %b%n",num1, num2, num1, num3,andOperator);
+		
+		boolean orOperator = (num1 > num2) || (num1 > num3);
+		System.out.printf("is (%d > %d) || (%d > %d): %b%n",num1, num2, num1, num3,orOperator);
+		
+		boolean notOperator = !((num1 > num2) || (num1 > num3));
+		System.out.printf("is !((%d > %d) || (%d > %d)): %b%n",num1, num2, num1, num3,notOperator);
+		
+		System.out.println("---------------------------------------------");
+		/*Unary Operators(++,--)
+		Pre-increment(++x) immediately increments the value of x by 1
+		post-increment(x++) increments the next time x is printed*/
+		int x = 5;
+		int y	= 10;
+		
+		System.out.printf("The value of x is %d%n",++x);
+		System.out.printf("The value of x is %d%n",y++);
+		System.out.printf("The value of x is %d%n",y);
+		System.out.printf("The value of x is %d%n",--x);
+		System.out.printf("The value of x is %d%n",y--);
+		System.out.printf("The value of x is %d%n",y);
+	}
+}
--- UserInput.java
+++ UserInput.java
@@ -7,6 +7,34 @@ public class UserInput{
 		System.out.print("Enter your name: ");
 		String name = input.nextLine();
 		
-		System.out.printf("Hello %s, you are welcome to NIIT.",name);
+		System.out.print("Enter your gender: ");
+		String gender = input.next();
+		
+		System.out.print("Enter your age: ");
+		byte age = input.nextByte();
+		
+		System.out.print("Enter the number of students in your class: ");
+		short numberOfStudents = input.nextShort();
+		
+		System.out.print("Enter the number of students in your School: ");
+		int totalNumberOfStudents = input.nextInt();
+		
+		System.out.print("Enter your grade: ");
+		char grade = input.next().charAt(0);
+		
+		System.out.print("Do you love learning Java(true/false): ");
+		boolean loveJava = input.nextBoolean();
+
+		System.out.println("");
+		System.out.printf("Information about %s%n",name);
+		System.out.println("----------------------------------------------");
+
+
+		System.out.printf("Hello %s, you are welcome to NIIT.%n",name);
+		System.out.printf("Your are a %s and you are %d years old%n", gender, age);
+		System.out.printf("There are %d students in your class%n",numberOfStudents);
+		System.out.printf("The total number of students in your school is %d%n",totalNumberOfStudents);
+		System.out.printf("Your grade is %c%n",grade);
+		System.out.printf("Do you love java? %b%n",loveJava);
 	}
 }
